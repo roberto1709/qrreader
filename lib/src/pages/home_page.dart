a171ed8a32ce3f9fad3b9a0c8cee65a281fb7505
roberto1709 <roberto.tavitas17@gmail.com>
2020-08-21 18:11:10 -0500

    First commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_pages.dart';
+import 'package:qrreaderapp/src/pages/mapas_pages.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _crearbottomNavigationBar(),
+    );
+  }
+
+  Widget _crearbottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Mapas')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_5), title: Text('Direcciones')),
+      ],
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+}
